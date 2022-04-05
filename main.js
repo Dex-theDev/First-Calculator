2d245ecccc22438e044268a6550398b28c435fc3
Jaime <jaime.joseph1991@gmail.com>
2022-04-04 21:34:16 -0400

    restarted the calculator and used cleaner code, added notes for later review

--- main.js
+++ main.js
@@ -1,50 +1,64 @@
-function Calculator(){
-    this.clear = function() {
-        display.innerHTML ='0'
-        return this
+document.querySelector('.body').addEventListener('click', event => {
+    // apparently this is called destructuring. I'll do more research later
+    const {target} = event;
+    const {value} = target;
+    if(!target.matches('button')){
+        return; //if the target that triggered the event is not a button, return nothing
+    } else {
+        //a function that adds the button value to the display. 
+        calculator.parseInput(value)
+        //console.log(target.value)
     }
-    //define variables according to the data attribute in the html file
-this.numberButtons = document.querySelectorAll('[data-number]')
-this.clearButton = document.querySelector('[data-clear')
-this.equalButton = document.querySelector('[data-equal]')
+})
+const calculator = {
+    displayText: '0',
+    prevTotal: null,
 
-this.display = document.querySelector('.body-display')
-this.buttons = document.querySelectorAll('button')
-this.operator = document.querySelectorAll('[data-operator')
-    
-numberButtons.forEach(e => e.addEventListener('click', function() {
-        //if(e === clearButton){ //added an if statement if the clear button was pressed.
-           //this.clear()
-         //}
-        
-        display.innerHTML += this.value //default action 
-    } ))
-    
-operator.forEach(e => e.addEventListener('click', function(){
-        display.innerHTML += this.value
-    }))
-clearButton.addEventListener('click',this.clear)
+    parseInput(value){
+       
+        switch(value){
+            //we need to account for the special buttons that can be pushed
+            case '=':
+                //calculate the answer
+                this.calcAnswer(this.displayText)
+                break;
+            case 'C':
+                //clear screen and stored values
+                break;
+            case '.':
+                if(this.displayText === 0){
+                    //pass '0.' into add text method
+                    this.addText('0.')
+                } else {
+                    //add value to text string
+                    this.addText(value)
+                }
+                break;
+                default: this.addText(value)//add value to text string
+                
+                
+            
+        }
+    },
+    addText(value){
+        if(this.displayText === '0'){ //this will ensure that the existing 0 will disappear once we start entering values.
+            this.displayText = ''
+        } else if (this.prevTotal !== null){
+            this.displayText = this.prevTotal
+            this.prevTotal = null
+        }
+        if(isNaN(+(value)) && isNaN(+(this.displayText))){
+            if(isNaN(this.displayText.slice(-1))){ //slice -1 indicates the last character of an array
+                return
+            }
+        }
+        this.displayText += value
+        this.outputText(this.displayText) //will output whatever the display text turns out to be
+    },
+    outputText(text){
+        document.querySelector('.body-display').innerHTML = text
+    },
+    calcAnswer(equation){
 
-    
+    }
 }
-
-
-//define variables according to the data attribute in the html file
-const numberButtons = document.querySelectorAll('[data-number]')
-const clearButton = document.querySelector('[data-clear')
-const equalButton = document.querySelector('[data-equal]')
-
-const display = document.querySelector('.body-display')
-const buttons = document.querySelectorAll('button')
-const operator = document.querySelectorAll('[data-operator')
-
-
-
-let calc = new Calculator()
-//let displayValue = 0
-// Add inner text to the display screen
-//display.innerHTML = displayValue
-
-// Now lets make these buttons actually put something on the screen. Using an Object
-
-
